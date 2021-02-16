7a39f76d598f46012aec09d244eb891fb45ad1a0
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 14:38:11 -0300

    Add files via upload

---
+++ Uri1072/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Uri1072
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, c, dentro, fora,num;
+
+            dentro = 0;
+            fora = 0;
+
+            n = int.Parse(Console.ReadLine());
+
+            while(n>10000)
+            {
+                Console.WriteLine("digite novamente");
+                n = int.Parse(Console.ReadLine());
+            }
+
+            for(c = 1; c<=n;c++)
+            {
+                num = int.Parse(Console.ReadLine());
+
+                if(num >=10&&num<=20)
+                {
+                    dentro += +1;
+                }
+                else
+                {
+                    fora += +1;
+                }
+            }
+
+
+            Console.WriteLine(dentro + " in");
+            Console.WriteLine(fora + " out");
+
+        }
+    }
+}
